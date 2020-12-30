5e271c9864965c42da2cd19e435f25ca4a78f8bd
Deepen Dhulla <deepen@deependhulla.com>
2020-12-30 06:24:03 +0000

    update 2020-12-30

--- 11-mailarchive-setup.sh
+++ 11-mailarchive-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get -y install recoll xapian-tools antiword catdoc unrar unrtf pdftk wv
+apt-get -y install recoll xapian-tools antiword catdoc rar unrar python3-rarfile python-future unrtf pdftk wv
 
 
 #apt-get install tesseract-ocr-all 
