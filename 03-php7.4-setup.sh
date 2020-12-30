f55325f2a21bc6cc06ee3866237acc02e810fe98
Deepen Dhulla <deepen@deependhulla.com>
2020-12-30 03:41:50 +0000

    update 2020-12-30

--- 03-php7.4-setup.sh
+++ 03-php7.4-setup.sh
@@ -7,7 +7,7 @@ wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add -
 apt update
 apt-get -y upgrade
 
-apt-get install -y  apache2 php7.4-fpm php7.4-imap php7.4-gd php7.4-mysql php7.4-curl php7.4-xml php7.4-zip php7.4-intl php7.4-mbstring php7.4-json php7.4-bz2 php7.4-ldap php7.4-apcu php7.4-cli php7.4-bcmath libapache2-mod-fcgid  libapache2-mod-php7.4
+apt-get install -y  apache2 php7.4-fpm php7.4-imap php7.4-mailparse php7.4-gd php7.4-mysql php7.4-curl php7.4-xml php7.4-zip php7.4-intl php7.4-mbstring php7.4-json php7.4-bz2 php7.4-ldap php7.4-apcu php7.4-cli php7.4-bcmath libapache2-mod-fcgid  libapache2-mod-php7.4
 
 
 a2enmod actions > /dev/null 2>&1
