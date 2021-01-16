7fb4fbaed7f4da50078fb3a8986c0c4548426f04
Deepen Dhulla <deepen@deependhulla.com>
2021-01-16 05:19:40 +0000

    update 2021-01-16

--- 11-mailarchive-setup.sh
+++ 11-mailarchive-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export RECOLL_TMPDIR=/archivedata/index-temp-folder/
+
 apt-get -y install recoll xapian-tools libdatetime-format-mail-perl 
 #apt-get install antiword catdoc python-future unrtf pdftk wv
 #apt-get install python3-rarfile 
